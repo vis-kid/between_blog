28d0592f2d6cc1cad1fba19d1850114f7b1ae9b5
ed wassermann <vis0vis@gmail.com>
2015-06-14 22:32:32 +0200

    Fixes whitespace

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-	var pipes = $('.list-keywords');
+  var pipes = $('.list-keywords');
   pipes.highlight('||');
-	var highlight_span = $('.highlight');
-	highlight_span.hide().fadeIn(2000);
+  var highlight_span = $('.highlight');
+  highlight_span.hide().fadeIn(2000);
 });
